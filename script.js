cfd3522766da7fe2c2b36992621740a9571a6ff3
Damilola Akinlade <akinlade.co@gmail.com>
2021-05-21 22:12:48 +0100

    commit

--- script.js
+++ script.js
@@ -15,44 +15,12 @@ const modal = document.querySelector('.modal');
 const modalFahrenheit = document.querySelector('.modal-fahrenheit');
 const modalYugioh = document.querySelector('.modal-yugioh');
 
-
+// modal text
+const modalTextFahrenheit = document.querySelector('.modal-text-fahrenheit');
+const modalTextYugioh = document.querySelector('.modal-text-yugioh');
 
 // overlay
 const overlay = document.querySelector('.overlay');
-
-// yugioh
-const checkYuGiOh = function (n) {
-	if (typeof n == 'number') {
-		const arr1 = Array.from({ length: n }, (_, i) => i + 1);
-		const arr2 = [];
-
-		for (let i = 0; i < arr1.length; i++) {
-			if (arr1[i] % 2 === 0) {
-				// arr2.push("-Yu");
-				arr2[i] = `Yu`;
-			}
-			if (arr1[i] % 3 === 0) {
-				// arr2.push('-Gi');
-				arr2[i] = arr2[i] ? `${arr2[i]}-Gi` : 'Gi';
-			}
-			if (arr1[i] % 5 === 0) {
-				// arr2.push('-Oh');
-				arr2[i] = arr2[i] ? `${arr2[i]}-Oh` : 'Oh';
-			}
-			if (!arr2[i]) {
-				arr2[i] = arr1[i];
-			}
-		}
-		arr1.map(val => {});
-
-		return arr2;
-	} else if (typeof n == 'string') {
-		return `${n} is not a valid number but a ${'string'}`;
-	} else {
-		return `${n} is not a valid number but a ${'boolean'}`;
-	}
-};
-console.log(checkYuGiOh(30));
 
 // close modal
 const closeModalFahrenheit = function () {
@@ -94,15 +62,51 @@ for (let i = 0; i < btn.length; i++) {
 		};
 
 		btn[i].addEventListener('click', function () {
-			// console.log(convertFahrToCelsius(Number(inputFahrenheit.value)));
 			console.log(convertFahrToCelsius());
+			modalTextFahrenheit.textContent = convertFahrToCelsius();
+
 			openModalFahrenheit();
 			overlay.addEventListener('click', closeModalFahrenheit);
 			closeBtnFahrenheit.addEventListener('click', closeModalFahrenheit);
 		});
 	}
+
 	if (btn[i].classList.contains('btn-yugioh')) {
+		// yugioh
+		const checkYuGiOh = function (n) {
+			n = Number(inputYugioh.value);
+			if (typeof n == 'number') {
+				const arr1 = Array.from({ length: n }, (_, i) => i + 1);
+				const arr2 = [];
+
+				for (let i = 0; i < arr1.length; i++) {
+					if (arr1[i] % 2 === 0) {
+						// arr2.push("-Yu");
+						arr2[i] = `Yu`;
+					}
+					if (arr1[i] % 3 === 0) {
+						// arr2.push('-Gi');
+						arr2[i] = arr2[i] ? `${arr2[i]}-Gi` : 'Gi';
+					}
+					if (arr1[i] % 5 === 0) {
+						// arr2.push('-Oh');
+						arr2[i] = arr2[i] ? `${arr2[i]}-Oh` : 'Oh';
+					}
+					if (!arr2[i]) {
+						arr2[i] = arr1[i];
+					}
+				}
+				arr1.map(val => {});
+
+				return arr2;
+			} else if (typeof n == 'string') {
+				return `${n} is not a valid number but a ${'string'}`;
+			} else {
+				return `${n} is not a valid number but a ${'boolean'}`;
+			}
+		};
 		btn[i].addEventListener('click', function () {
+			modalTextYugioh.textContent = checkYuGiOh();
 			openModalYugioh();
 			overlay.addEventListener('click', closeModalYugioh);
 			closeBtnYugioh.addEventListener('click', closeModalYugioh);
