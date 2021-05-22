b3316c9e2e8133b5f39d7069d2b760c275cbae81
Damilola Akinlade <akinlade.co@gmail.com>
2021-05-22 06:45:43 +0100

    commit

--- script.js
+++ script.js
@@ -45,74 +45,113 @@ const openModalYugioh = function () {
 	overlay.classList.remove('hidden');
 };
 
-// main codeblock
-for (let i = 0; i < btn.length; i++) {
-	if (btn[i].classList.contains('btn-fahrenheit')) {
-		// fahrenheit to celsius conversion
-		const convertFahrToCelsius = function (fahrenheit) {
-			fahrenheit = Number(inputFahrenheit.value);
-			if (typeof fahrenheit == 'number') {
-				const x = fahrenheit - 32;
-				const y = 5 / 9;
-				const z = x * y;
-				return z.toFixed(4) + '°C';
-			} else if (typeof fahrenheit == 'string') {
-				return `${fahrenheit} is not a valid number but a ${'string'}`;
-			} else {
-				return `${fahrenheit} is not a valid number but a ${'boolean'}`;
-			}
-		};
-
-		btn[i].addEventListener('click', function () {
-			modalTextFahrenheit.textContent = convertFahrToCelsius();
-			inputFahrenheit.value = '';
-
-			openModalFahrenheit();
-			overlay.addEventListener('click', closeModalFahrenheit);
-			closeBtnFahrenheit.addEventListener('click', closeModalFahrenheit);
-		});
+// fahrenheit to celsius conversion
+const convertFahrToCelsius = function (fahrenheit) {
+	fahrenheit = Number(inputFahrenheit.value);
+	if (typeof fahrenheit == 'number') {
+		const x = fahrenheit - 32;
+		const y = 5 / 9;
+		const z = x * y;
+		return z.toFixed(4) + '°C';
+	} else if (typeof fahrenheit == 'string') {
+		return `${fahrenheit} is not a valid number but a ${'string'}`;
+	} else {
+		return `${fahrenheit} is not a valid number but a ${'boolean'}`;
 	}
+};
 
-	if (btn[i].classList.contains('btn-yugioh')) {
-		// yugioh
-		const checkYuGiOh = function (n) {
-			n = Number(inputYugioh.value);
-			if (typeof n == 'number') {
-				const arr1 = Array.from({ length: n }, (_, i) => i + 1);
-				const arr2 = [];
+// display result for fahrenheit conversion
+const resultFahrenheit = function () {
+	modalTextFahrenheit.textContent = convertFahrToCelsius();
+	inputFahrenheit.value = '';
 
-				for (let i = 0; i < arr1.length; i++) {
-					if (arr1[i] % 2 === 0) {
-						arr2[i] = `Yu`;
-					}
-					if (arr1[i] % 3 === 0) {
-						arr2[i] = arr2[i] ? `${arr2[i]}-Gi` : 'Gi';
-					}
-					if (arr1[i] % 5 === 0) {
-						arr2[i] = arr2[i] ? `${arr2[i]}-Oh` : 'Oh';
-					}
-					if (!arr2[i]) {
-						arr2[i] = arr1[i];
-					}
-				}
+	openModalFahrenheit();
+	overlay.addEventListener('click', closeModalFahrenheit);
+	closeBtnFahrenheit.addEventListener('click', closeModalFahrenheit);
+};
 
-				return arr2;
-			} else if (typeof n == 'string') {
-				return `${n} is not a valid number but a ${'string'}`;
-			} else {
-				return `${n} is not a valid number but a ${'boolean'}`;
+// yugioh func
+const checkYuGiOh = function (n) {
+	n = Number(inputYugioh.value);
+	if (typeof n == 'number') {
+		const arr1 = Array.from({ length: n }, (_, i) => i + 1);
+		const arr2 = [];
+
+		for (let i = 0; i < arr1.length; i++) {
+			if (arr1[i] % 2 === 0) {
+				arr2[i] = `Yu`;
 			}
-		};
-		btn[i].addEventListener('click', function () {
-			modalTextYugioh.textContent = checkYuGiOh();
-			inputYugioh.value = '';
+			if (arr1[i] % 3 === 0) {
+				arr2[i] = arr2[i] ? `${arr2[i]}-Gi` : 'Gi';
+			}
+			if (arr1[i] % 5 === 0) {
+				arr2[i] = arr2[i] ? `${arr2[i]}-Oh` : 'Oh';
+			}
+			if (!arr2[i]) {
+				arr2[i] = arr1[i];
+			}
+		}
 
-			openModalYugioh();
-			overlay.addEventListener('click', closeModalYugioh);
-			closeBtnYugioh.addEventListener('click', closeModalYugioh);
-		});
+		return arr2;
+	} else if (typeof n == 'string') {
+		return `${n} is not a valid number but a ${'string'}`;
+	} else {
+		return `${n} is not a valid number but a ${'boolean'}`;
 	}
-}
+};
+
+const resultYugioh = function () {
+	modalTextYugioh.textContent = checkYuGiOh();
+	inputYugioh.value = '';
+
+	openModalYugioh();
+	overlay.addEventListener('click', closeModalYugioh);
+	closeBtnYugioh.addEventListener('click', closeModalYugioh);
+};
+
+// condition
+const init = (function () {
+	for (let i = 0; i < btn.length; i++) {
+		if (btn[i].classList.contains('btn-fahrenheit')) {
+			convertFahrToCelsius();
+			btn[i].addEventListener('click', function () {
+				resultFahrenheit();
+			});
+		}
+
+		if (btn[i].classList.contains('btn-yugioh')) {
+			// call yugioh func
+			checkYuGiOh();
+			btn[i].addEventListener('click', function () {
+				resultYugioh();
+			});
+		}
+	}
+})();
+
+// Execute function when the user releases a key on the keyboard
+inputFahrenheit.addEventListener('keyup', function (event) {
+	// Number 13 is the "Enter" key on the keyboard
+	if (event.key === 'Enter') {
+		// Cancel the default action, if needed
+		event.preventDefault();
+
+		convertFahrToCelsius();
+		resultFahrenheit();
+	}
+});
+
+// Execute function when the user releases a key on the keyboard
+inputYugioh.addEventListener('keyup', function (event) {
+	// Number 13 is the "Enter" key on the keyboard
+	if (event.key === 'Enter') {
+		// Cancel the default action, if needed
+		event.preventDefault();
+
+		checkYuGiOh();
+		resultYugioh();
+	}
+});
 
 // close modal with esc key
 document.addEventListener('keydown', function (event) {
